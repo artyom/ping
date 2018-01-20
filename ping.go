eb7498c881e7958e36a903610fbb2d5afbf5bca3
Artyom Pervukhin <artyom@evasive.ru>
2018-01-20 13:10:20 +0300

    Refactor code to be more composable

--- ping.go
+++ ping.go
@@ -1,16 +1,14 @@
+// Package ping provides primitives to ping network endpoints.
 package ping
 
 import (
 	"bytes"
-	"context"
+	"crypto/rand"
 	"encoding/binary"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"math"
 	"net"
-	"os"
+	"sync"
 	"time"
 
 	"golang.org/x/net/icmp"
@@ -26,25 +24,133 @@ type Summary struct {
 	DevRTT     time.Duration
 }
 
-func (s Summary) Stat() string {
-	var pct float64
-	if s.Sent > 0 && s.Lost > 0 {
-		pct = float64(s.Lost) / float64(s.Sent) * 100
-	}
-	return fmt.Sprintf("%d packets transmitted, %d packets received, %.1f%% packet loss\n"+
-		"round-trip min/avg/max/stddev = %v/%v/%v/%v", s.Sent, s.Sent-s.Lost, pct,
-		s.MinRTT.Truncate(time.Microsecond),
-		s.AvgRTT.Truncate(time.Microsecond),
-		s.MaxRTT.Truncate(time.Microsecond),
-		s.DevRTT.Truncate(time.Microsecond),
-	)
+// Pinger is used to ping remote endpoints.
+type Pinger interface {
+	// Ping sends single packet and waits for a limited time for reply.
+	// Returned values are whether reply was received in time, measured
+	// round trip time, number of bytes in received payload and error. It
+	// only returns errors on non-recoverable errors, if it does not receive
+	// reply for implementation-specific time limit, it reports false with
+	// nil error which should be interpreted as timeout.
+	Ping() (ok bool, rtt time.Duration, bytesRead int, err error)
+
+	// PeerIP returns IP address Ping sends packets to
+	PeerIP() net.IP
+
+	// Summary returns statistics collected from multiple Ping calls. It is
+	// usually called after multiple Ping calls and followed by Close call.
+	Stat() Summary
+
+	// Close frees resources used by Pinger (network sockets, etc.) Other
+	// methods should not be called after Close.
+	Close() error
 }
 
-// ICMP pings addr using IPv4 ICMP echo messages until count packets are sent or
-// ctx is canceled. If w is not null, output similar to ping(8) command is
-// written there. If count is not positive, function runs until ctx is canceled.
-// If addr is not an IPv4 address, it is resolved and first IPv4 record is used.
-func ICMP(ctx context.Context, w io.Writer, count int, addr string) (*Summary, error) {
+type icmpPinger struct {
+	mu sync.Mutex
+	Summary
+	udpAddr *net.UDPAddr
+	conn    *icmp.PacketConn
+	msgID   int // ping session id
+	seq     int // sequence number
+
+	m, s, k int64 // for stddev calculations
+
+	rttSum time.Duration
+	rcvBuf []byte
+	sndBuf []byte
+}
+
+func (p *icmpPinger) PeerIP() net.IP {
+	if p.udpAddr != nil {
+		return p.udpAddr.IP
+	}
+	return nil
+}
+
+func (p *icmpPinger) Ping() (ok bool, rtt time.Duration, bytesRead int, err error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	start := time.Now()
+	binary.LittleEndian.PutUint64(p.sndBuf[3:], uint64(start.UnixNano()))
+	msg := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Body: &icmp.Echo{ID: p.msgID, Seq: p.seq, Data: p.sndBuf},
+	}
+	b, err := msg.Marshal(nil)
+	if err != nil {
+		return false, 0, 0, err
+	}
+	sendTime := time.Now()
+	if err := p.conn.SetWriteDeadline(sendTime.Add(time.Second)); err != nil {
+		return false, 0, 0, err
+	}
+	if _, err := p.conn.WriteTo(b, p.udpAddr); err != nil {
+		return false, 0, 0, err
+	}
+	p.Summary.Sent++
+	if err := p.conn.SetReadDeadline(time.Now().Add(time.Second)); err != nil {
+		return false, 0, 0, err
+	}
+
+	for {
+		n, _, err := p.conn.ReadFrom(p.rcvBuf)
+		if te, ok := err.(interface{ Timeout() bool }); ok && te.Timeout() {
+			p.Summary.Lost++
+			return false, 0, 0, nil
+		}
+		if err != nil {
+			return false, 0, 0, err
+		}
+		rtt := time.Since(sendTime)
+		msg2, err := icmp.ParseMessage(1, p.rcvBuf[:n])
+		if err != nil {
+			continue
+		}
+		if msg2.Type != ipv4.ICMPTypeEchoReply {
+			continue
+		}
+		if body, ok := msg2.Body.(*icmp.Echo); ok &&
+			body.ID == p.msgID &&
+			body.Seq == p.seq &&
+			bytes.Equal(body.Data, p.sndBuf) {
+			{
+				if p.k == 0 {
+					p.k = 1
+				}
+				val := int64(rtt)
+				_m := p.m
+				p.m += (val - _m) / p.k
+				p.s += (val - _m) * (val - p.m)
+				p.k++
+			}
+			if p.Summary.MaxRTT == 0 || p.Summary.MaxRTT < rtt {
+				p.Summary.MaxRTT = rtt
+			}
+			if p.Summary.MinRTT == 0 || p.Summary.MinRTT > rtt {
+				p.Summary.MinRTT = rtt
+			}
+			p.rttSum += rtt
+			p.Summary.AvgRTT = p.rttSum / time.Duration(p.Summary.Sent)
+			return true, rtt, n, nil
+		}
+	}
+}
+
+func (p *icmpPinger) Close() error { return p.conn.Close() }
+
+func (p *icmpPinger) Stat() Summary {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	summary := p.Summary
+	summary.DevRTT = time.Duration(int64(math.Sqrt(float64(p.s / (p.k - 1)))))
+	return summary
+}
+
+// NewICMP returns new Pinger which pings addr using IPv4 ICMP echo messages.
+// addr should either be an IPv4 or hostname that resolves to IPv4. If addr
+// resolves to multiple addresses, the first IPv4 record is used.
+func NewICMP(addr string) (Pinger, error) {
 	dst := net.ParseIP(addr)
 	if dst == nil {
 		ips, err := net.LookupIP(addr)
@@ -61,115 +167,19 @@ func ICMP(ctx context.Context, w io.Writer, count int, addr string) (*Summary, e
 	if dst == nil {
 		return nil, errors.New("cannot resolve address to ipv4")
 	}
-	dstAddr := &net.UDPAddr{IP: dst}
-
 	c, err := icmp.ListenPacket("udp4", "0.0.0.0")
 	if err != nil {
 		return nil, err
 	}
-	defer c.Close()
-	if deadline, ok := ctx.Deadline(); ok {
-		c.SetDeadline(deadline)
-	}
-	if w == nil {
-		w = ioutil.Discard
-	}
-
-	rcvBuf := make([]byte, 1500)
-	msgID := os.Getpid() & 0xffff // FIXME
-	buf := make([]byte, payloadSize)
-	_ = append(buf[:0], "ping"...)
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	var summary Summary
-	var rttSum time.Duration
-	// Welford's method: https://stackoverflow.com/a/897463/229034
-	// https://www.johndcook.com/blog/standard_deviation/
-	var m, s, k int64 = 0, 0, 1
-sendLoop:
-	for seq := 0; ; seq++ {
-		if count > 0 && seq == count {
-			break
-		}
-		var start time.Time
-		switch seq {
-		case 0:
-			start = time.Now()
-		default:
-			select {
-			case start = <-ticker.C:
-			case <-ctx.Done():
-				break sendLoop
-
-			}
-		}
-		binary.LittleEndian.PutUint64(buf[3:], uint64(start.UnixNano()))
-		msg := icmp.Message{
-			Type: ipv4.ICMPTypeEcho,
-			Body: &icmp.Echo{ID: msgID, Seq: seq, Data: buf},
-		}
-		b, err := msg.Marshal(nil)
-		if err != nil {
-			return nil, err
-		}
-		sendTime := time.Now()
-		if _, err := c.WriteTo(b, dstAddr); err != nil {
-			return nil, err
-		}
-		summary.Sent++
-		if err := c.SetReadDeadline(time.Now().Add(time.Second)); err != nil {
-			return nil, err
-		}
-
-		for {
-			n, remoteAddr, err := c.ReadFrom(rcvBuf)
-			if te, ok := err.(interface{ Timeout() bool }); ok && te.Timeout() {
-				fmt.Fprintln(w, "Request timeout for icmp_seq", seq) // FIXME
-				summary.Lost++
-				continue sendLoop
-			}
-			if err != nil {
-				return nil, err
-			}
-			rtt := time.Since(sendTime)
-			msg2, err := icmp.ParseMessage(1, rcvBuf[:n])
-			if err != nil {
-				continue
-			}
-			if msg2.Type != ipv4.ICMPTypeEchoReply {
-				continue
-			}
-			if body, ok := msg2.Body.(*icmp.Echo); ok &&
-				body.ID == msgID &&
-				body.Seq == seq &&
-				bytes.Equal(body.Data, buf) {
-				{
-					val := int64(rtt)
-					_m := m
-					m += (val - _m) / k
-					s += (val - _m) * (val - m)
-					k++
-				}
-				if summary.MaxRTT == 0 || summary.MaxRTT < rtt {
-					summary.MaxRTT = rtt
-				}
-				if summary.MinRTT == 0 || summary.MinRTT > rtt {
-					summary.MinRTT = rtt
-				}
-				rttSum += rtt
-				summary.AvgRTT = rttSum / time.Duration(summary.Sent)
-				fmt.Fprintf(w, "%d bytes from %s: icmp_seq=%d rtt=%v\n",
-					n, remoteAddr.(*net.UDPAddr).IP,
-					seq, rtt.Truncate(time.Microsecond))
-				continue sendLoop
-			}
-		}
-	}
-	summary.DevRTT = time.Duration(int64(math.Sqrt(float64(s / (k - 1)))))
-
-	fmt.Fprintf(w, "--- %s ping statistics ---\n", addr)
-	fmt.Fprintln(w, summary.Stat())
-	return &summary, nil
+	sndBuf := make([]byte, 56)
+	_ = append(sndBuf[:0], "ping"...)
+	b := make([]byte, 2)
+	rand.Read(b)
+	return &icmpPinger{
+		udpAddr: &net.UDPAddr{IP: dst},
+		conn:    c,
+		msgID:   int(binary.LittleEndian.Uint16(b)),
+		rcvBuf:  make([]byte, 1500),
+		sndBuf:  sndBuf,
+	}, nil
 }
-
-const payloadSize = 56
